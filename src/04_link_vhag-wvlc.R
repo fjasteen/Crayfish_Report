f0b81c99c27a663d3cbd27a0b621752037f23ea8
frederiquest <69361920+frederiquest@users.noreply.github.com>
2025-12-08 22:57:59 +0100

    Update 04 with helper-function
    for linkage to VHAG/WVLC

--- src/04_link_vhag-wvlc.R
+++ src/04_link_vhag-wvlc.R
@@ -7,6 +7,7 @@
 
 # --- 0. Setup ---
 source("./src/config.R")
+source("./src/helpers.R")
 library(lwgeom) 
 library(purrr)
 
@@ -33,107 +34,46 @@ message(paste("Analysedataset geladen:", nrow(dataset_sf), "rijen."))
 # --- 2. Initiële Koppeling (Nearest Feature) ---
 message("Uitvoeren koppeling met WVLC/VHAS...")
 
-# We zoeken nu in de GEDETAILLEERDE segmenten
-idx_riv <- st_nearest_feature(dataset_sf, waterloopsegmenten)
-idx_wat <- st_nearest_feature(dataset_sf, watervlakken)
+# 2. Uitvoeren van de functie
+# Dit vervangt regels 40 t/m 115 van je oude script!
+dataset_linked <- match_points_to_water(
+  points_sf = dataset_sf,
+  rivers = waterloopsegmenten,
+  lakes = watervlakken,
+  watergang = watergang,
+  buffer_m = max_link_distance_m # Uit config.R
+)
 
-dataset_linked <- dataset_sf %>%
-  mutate(
-    # Kandidaat open (uit waterloopsegmenten)
-    VHAG_cand = waterloopsegmenten$VHAG[idx_riv],
-    VHAS_cand = waterloopsegmenten$VHAS[idx_riv],
-    # Check of CATC aanwezig is in segmenten file, anders later joinen
-    CATC_cand = if("CATC" %in% names(waterloopsegmenten)) waterloopsegmenten$CATC[idx_riv] else NA,
-    dist_riv  = as.numeric(st_distance(geometry, waterloopsegmenten[idx_riv, ], by_element = TRUE)),
-    
-    # Kandidaat gesloten
-    WVLC_cand = watervlakken$WVLC[idx_wat],
-    dist_wat  = as.numeric(st_distance(geometry, watervlakken[idx_wat, ], by_element = TRUE)),
-    
-    # Bepaal type en afstand
-    type_water  = if_else(dist_riv <= dist_wat, "open", "gesloten"),
-    dist_actual = pmin(dist_riv, dist_wat)
+# 3.  CATC erbij halen 
+vha_waterloop <- read_sf(file_vha_catc) %>% st_transform(31370) %>% st_drop_geometry()
+
+dataset_final <- dataset_linked %>%
+  left_join(
+    waterloopsegmenten %>% 
+      st_drop_geometry() %>% 
+      mutate(VHAS = as.character(VHAS)) %>%  # <--- HIER: Converteer naar tekst
+      select(VHAS, CATC), 
+    by = "VHAS"
   )
 
-# --- 3. Validatie & filtering ---
-message("Toepassen validatie logica...")
 
-buffer <- max_link_distance_m
+# 4. Export
+write.csv(st_drop_geometry(dataset_final), file_analyse_dataset_rapport, row.names = FALSE)
+message("Script 04 Klaar.")
 
-# A. VHAS/WVLC binnen afstand buffer
-data_strict <- dataset_linked %>%
-  filter(dist_actual <= buffer) %>%
-  mutate(
-    valid_link = TRUE, # We nemen aan dat binnen buffer altijd OK is (optie 'benefit of doubt')
-    link_method = "<buffer"
-  )
-
-# B. VHAS buiten afstand buffer -> naar GRB logica
-data_candidates <- dataset_linked %>%
-  filter(dist_actual > buffer & type_water == "open")
-
-# C. WVLC niet binnen afstand buffer 
-data_invalid <- dataset_linked %>%
-  filter(dist_actual > buffer & type_water == "gesloten") %>%
-  mutate(valid_link = FALSE, link_method = "not linked")
-
-# Verwerking GRB kandidaten
-if(nrow(data_candidates) > 0) {
-  
-  data_grb_checked <- data_candidates %>%
-    st_join(watergang, join = st_nearest_feature, suffix = c("", "_wg")) %>%
-    mutate(
-      vhag_from_grb = VHAG, 
-      is_same_vhag = if_else(!is.na(vhag_from_grb), 
-                             as.character(VHAG_cand) == as.character(vhag_from_grb), 
-                             TRUE), 
-      # Buffer berekening
-      breedteschatting = OPPERVL / LENGTE,
-      max_buffer       = (breedteschatting / 2) + buffer,
-      # Dubbele validatie: afstand binnen buffer
-      valid_link  = (dist_actual <= max_buffer) & is_same_vhag,
-      link_method = if_else(valid_link, "GRB/2 + buffer", "not linked")
-    ) %>%
-    # Ruim GRB kolommen op (incl de tijdelijke vhag_from_grb)
-    select(any_of(colnames(dataset_linked)), valid_link, link_method)
-  
-} else {
-  data_grb_checked <- data_candidates %>% mutate(valid_link = FALSE, link_method = NA)
-}
-
-# --- 4. Finaliseren ---
-message("Samenvoegen en opschonen...")
-
-dataset_final <- bind_rows(data_strict, data_grb_checked, data_invalid) %>%
-  arrange(locID, date) %>%
-  mutate(
-    VHAG = if_else(valid_link & type_water == "open", as.character(VHAG_cand), NA_character_),
-    VHAS = if_else(valid_link & type_water == "open", as.character(VHAS_cand), NA_character_),
-    CATC = if_else(valid_link & type_water == "open", as.character(CATC_cand), NA_character_),
-    WVLC = if_else(valid_link & type_water == "gesloten", as.character(WVLC_cand), NA_character_),
-    distances = if_else(valid_link, dist_actual, NA_real_)
-  ) %>%
-  select(-ends_with("_cand"), -dist_riv, -dist_wat, -dist_actual, -type_water)
-
-# --- 5. Export ---
 # Stats printen
 cat(paste0("Totaal records: ", nrow(dataset_final), "\n"))
 cat(paste0("Succesvol gekoppeld: ", sum(dataset_final$valid_link), "\n"))
 print(table(dataset_final$link_method[dataset_final$valid_link]))
 
 # Visuele check (Histogram)
-p <- ggplot(dataset_final %>% filter(valid_link), aes(x = distances)) +
+p <- ggplot(dataset_final %>% filter(valid_link), aes(x = distance_linked)) +
   geom_histogram(binwidth = 1, fill = "steelblue", color = "white") +
-  geom_vline(xintercept = buffer, color = "red", linetype = "dashed") +
+  geom_vline(xintercept = max_link_distance_m, color = "red", linetype = "dashed") +
   labs(title = "Afstanden gevalideerde koppelingen", x = "Afstand (m)")
 ggsave(file.path(dir_data_intermediate, "check_spatial_link_distances.png"), p, width = 8, height = 6)
 
-# Opslaan
-# Indien output paden niet in config stonden, definieer ze hier even tijdelijk:
-write.csv(dataset_final %>% st_drop_geometry(), 
-          file = file_analyse_dataset_rapport, 
-          quote = TRUE, 
-          row.names = FALSE)
+
 
 message(paste("Dataset opgeslagen in rapport map:", file_analyse_dataset_rapport))
 
@@ -161,7 +101,7 @@ map_sf <- dataset_final %>%
       TRUE ~ "Niet gekoppeld"
     ),
     # 3. Afstand afronden voor nette popup (1 decimaal)
-    Afstand_m = round(distances, 1)
+    Afstand_m = round(distance_linked, 1)
   ) %>%
   
   # 4. Omzetten naar SF
